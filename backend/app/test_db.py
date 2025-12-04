fddd47c401c55aee9c0cdec62b0242edb2a627ac
Shivamk0107 <75519606+Shivamk0107@users.noreply.github.com>
2025-12-04 22:18:08 +0530

    Update test_db.py

--- backend/app/test_db.py
+++ backend/app/test_db.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import asyncio
 
 async def test_connection():
-    MONGO_URL = "mongodb+srv://fitflowuser:Fitflow123@fitflowhub.qwert.mongodb.net/fitflowhub"
+    MONGO_URL = ""
     client = AsyncIOMotorClient(MONGO_URL)
     db = client["fitflowhub"]
     print(await db.list_collection_names())
